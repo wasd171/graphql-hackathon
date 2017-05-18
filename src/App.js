6989203712be873cf99171b6a7da6111c8745b76
Konstantin Nesterov <wasd171@gmail.com>
2017-05-18 19:38:40 +0200

    Added tooltips

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components'
 import { graphql, gql } from 'react-apollo'
-import { VictoryChart, VictoryLine, VictoryLabel, VictoryLegend } from 'victory'
+import { VictoryChart, VictoryLine, VictoryLabel, VictoryLegend, VictoryTooltip, VictoryScatter } from 'victory'
 import MadeWithLove from 'react-made-with-love'
 
 const Container = styled.div`
@@ -20,8 +20,10 @@ const Wrapper = styled.div`
 const query = gql`
   query mainQuery {
     Repository {
-      size
+      _size: size
       open_issues
+      url
+      full_name
       owner {
         type
       }
@@ -43,19 +45,48 @@ const options = {
   })
 }
 
+const USER_COLOR = '#0b3536';
+const ORGANISATION_COLOR = '#f54123';
+const STROKE_WIDTH = 0.5;
+const POINT_SIZE = 2;
+
 const styles = {
   users: {
-    data: {
-      stroke: '#0b3536',
-      strokeWidth: 1
+    line: {
+      data: {
+        stroke: USER_COLOR,
+        strokeWidth: STROKE_WIDTH
+      }
+    },
+    point: {
+      data: {
+        fill: USER_COLOR
+      }
     }
   },
   organisations: {
-    data: {
-      stroke: '#f54123',
-      strokeWidth: 1
+    line: {
+      data: {
+        stroke: ORGANISATION_COLOR,
+        strokeWidth: STROKE_WIDTH
+      }
+    },
+    point: {
+      data: {
+        fill: ORGANISATION_COLOR
+      }
     }
   }
+}
+
+const Tooltip = <VictoryTooltip 
+  style={{
+    fontSize: 8
+  }}
+/>
+
+function getLabel(repo) {
+  return repo.full_name
 }
 
 class App extends Component {
@@ -64,20 +95,38 @@ class App extends Component {
       <VictoryLabel x={25} y={24} text="How does the number of open issues depend on the size of repository?"/>
       <VictoryLine
         data={this.props.repos.users}
-        x="size"
+        x="_size"
         y="open_issues"
-        style={styles.users}
+        style={styles.users.line}
+      />
+      <VictoryScatter
+        data={this.props.repos.users}
+        x="_size"
+        y="open_issues"
+        style={styles.users.point}
+        size={POINT_SIZE}
+        labels={getLabel}
+        labelComponent={Tooltip}
       />
       <VictoryLine
         data={this.props.repos.organisations}
-        x="size"
+        x="_size"
         y="open_issues"
-        style={styles.organisations}
+        style={styles.organisations.line}
+      />
+      <VictoryScatter
+        data={this.props.repos.organisations}
+        x="_size"
+        y="open_issues"
+        style={styles.organisations.point}
+        size={POINT_SIZE}
+        labels={getLabel}
+        labelComponent={Tooltip}
       />
       <VictoryLegend
         colorScale={[
-          styles.users.data.stroke,
-          styles.organisations.data.stroke
+          USER_COLOR,
+          ORGANISATION_COLOR
         ]}
         data={[
           {name: 'Users'},
